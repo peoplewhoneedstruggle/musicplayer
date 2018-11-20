ef5cccc30fbec0cb837f13dfd574aebfee40824b
peoplewhoneedstruggle <1711883743@qq.com>
2018-11-21 01:54:28 +0800

    首页骨架

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-// import router from './router'
+import router from './router'
 import fastclick from 'fastclick'
 // Vue.config.productionTip = false
 import 'common/stylus/index.styl'
@@ -11,5 +11,6 @@ fastclick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
