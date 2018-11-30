3906f930ed32853eeb57c3e027d60abc525d7875
mwl1711883743 <1711883743@qq.com>
2018-11-30 18:07:05 +0800

    歌单

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -1,12 +1,41 @@
 <template>
   <div>
-    排行页面
+    <div v-for="(item, index) in rank" :key=index class="rank">
+      <div>
+        <img :src="item.coverImgUrl" class="rank__img" alt="排行榜图片">
+      </div>
+    </div>
   </div>
 </template>
 <script>
 export default {
+  mounted () {
+    this.getRank()
+  },
+  data () {
+    return {
+      rank: []
+    }
+  },
+  methods: {
+    getRank () {
+      let _this = this
+      this.$.ajax({
+        url: 'http://localhost:3000/toplist',
+        type: 'get',
+        data: '',
+        success (data) {
+          _this.rank.splice(0, 0, ...data.list)
+          console.log(_this.rank)
+        }
+      })
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped rel="stylesheet/stylus">
-
+.rank
+  width 10rem
+.rank__img
+  width 100%
 </style>
